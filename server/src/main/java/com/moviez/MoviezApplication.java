21423535d16540212dd0c5d9700e7e98bb8c75b3
sourabh-yalagod <sourabhofficial99804@gmail.com>
2026-03-03 14:33:34 +0530

    server init

---
+++ server/src/main/java/com/moviez/MoviezApplication.java
@@ -0,0 +1,13 @@
+package com.moviez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviezApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/moviez/MoviezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
